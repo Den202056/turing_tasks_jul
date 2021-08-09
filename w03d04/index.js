fdd3ee56f153d8499d156c50731e0edc796a4ea1
Den202056 <jsworkmail19@gmail.com>
2021-08-09 10:04:59 +0500

    Week 3 example 4 pull request

--- w03d04/index.js
+++ w03d04/index.js
@@ -6,17 +6,12 @@ const obj = {
 }
 
 function listPropertyObject (obj1) {
-    for (let [key, value] of obj1) {
-        console.log( );
+    for (let key in obj1) {
+        console.log(`${key} - ${obj1[key]}`);
     }
 }
 
-// function listPropertyObject (obj1) {
-//     for (let key in obj1) {
-//         console.log(`${key} - ${obj1[key]}`);
-//     }
-// }
-
+console.log('Part 01');
 listPropertyObject(obj);
 
     //Part 02
@@ -26,6 +21,7 @@ function listAllPropertyObject (obj1) {
     }
 }
 
+console.log('Part 02');
 listAllPropertyObject(obj);
 
     //Part 03
@@ -33,4 +29,49 @@ const mas01 = [1, 2, 3];
 const mas02 = [4, 5, 6];
 const mas03 = [7, 8, 9];
 
-const map01 = ()
+let map = new Map([
+    [mas01, '123'],
+    [mas02, '456'],
+    [mas03, '789']
+])
+
+console.log('Part 03');
+
+    //Part 04
+console.log('Part 04')
+
+const keysList = map.keys();
+
+for (let key of keysList)
+    console.log(key);
+
+    //Part 05
+console.log('Part 05')
+let set = new Set();
+
+set.add(1);
+set.add(2);
+set.add(3);
+
+    //Part 06
+console.log('Part 06')
+console.log(set.has(3));
+console.log(set.has(4));
+
+    //Part 07
+console.log('Part 07');
+let mas = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5];
+mas = new Set(mas);
+console.log(mas);
+
+    //Part 08
+console.log('Part 08');
+let map2 = new Map([
+    ['Monday', 'Понедельник'],
+    ['Tuesday', 'Вторник'],
+    ['Wednesday', 'Среда'],
+    ['Thursday', 'Четверг'],
+    ['Friday', 'Пятница'],
+    ['Saturday', 'Суббота'],
+    ['Sunday', 'Воскресенье']
+]);
